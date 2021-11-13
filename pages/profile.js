5724d486056527b20920f39afddd9e5a8e5f79cf
serebano <sergiu.toderascu@gmail.com>
2021-11-13 02:10:58 +0200

    added account & avatar

--- pages/profile.js
+++ pages/profile.js
@@ -1,27 +1,31 @@
 import { Auth, Typography, Button } from "@supabase/ui";
 const { Text } = Typography
+import Account from '../components/account'
+
 import { supabase } from '../api'
 
 function Profile(props) {
-    const { user } = Auth.useUser();
-    if (user)
-      return (
-        <>
-          <Text>Signed in: {user.email}</Text>
-          <Button block onClick={() => props.supabaseClient.auth.signOut()}>
-            Sign out
-          </Button>
-        </>
-      );
-    return props.children 
+  const { user } = Auth.useUser();
+  const session = supabase.auth.session()
+  if (user)
+    return (
+      <>
+        <Account key={session.user.id} session={session} />
+        <Text>Signed in: {user.email}</Text>
+        <Button block onClick={() => props.supabaseClient.auth.signOut()}>
+          Sign out
+        </Button>
+      </>
+    );
+  return props.children
 }
 
 export default function AuthProfile() {
-    return (
-        <Auth.UserContextProvider supabaseClient={supabase}>
-          <Profile supabaseClient={supabase}>
-            <Auth supabaseClient={supabase} />
-          </Profile>
-        </Auth.UserContextProvider>
-    )
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Profile supabaseClient={supabase}>
+        <Auth supabaseClient={supabase} />
+      </Profile>
+    </Auth.UserContextProvider>
+  )
 }
